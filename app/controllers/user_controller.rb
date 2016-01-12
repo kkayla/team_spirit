89dcdee4a7f01408aacf5211fc3d4afa7742d95d
Sarah Corey <syncorey@gmail.com>
2016-01-12 16:41:27 -0500

    Add User Controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,25 @@
 class UserController < ApplicationController
+
+def new
+	@user=User.new
+end
+
+def create
+		
+		@user = User.new(params[:user])
+	  if @user.save
+	    redirect_to user_path @user
+	  else
+	  end
+	end
+
+def show
+	@user=User.find(params[:id])
+end
+
+def create
+	@user=User.new(params[:user])
+end
+
+
 end
